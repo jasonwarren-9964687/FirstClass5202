b8dbda239ea33a5cbcad738a52d656fe9ac29d0f
Jason Warren <9964687@student.toiohomai.ac.nz>
2019-07-24 11:27:28 +1200

    First app thing

---
+++ 5202Wk1A/5202Wk1A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5202Wk1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable 0
+            Console.WriteLine("Enter First Name");
+            string name = Console.ReadLine();
+            //Variable 1
+            Console.WriteLine("Enter Last Name");
+            string lname = Console.ReadLine();
+            //Outputs
+            Console.WriteLine("Hello {0} {1}, nice to meet you.", name,lname);
+            Console.WriteLine("Hello " + name + " " + lname + ", nice to meet you.");
+            Console.WriteLine($"Hello {name} {lname}, nice to meet you.");
+            Console.ReadLine();
+            
+        }
+    }
+}
